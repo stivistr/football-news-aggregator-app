e05b98b5c51a0864f0e3f811bb48e0d47593170c
Stivan Strahilov <stivan_strahilov@abv.bg>
2023-07-07 23:03:02 +0300

    css and base html added

--- football_news_aggregator/accounts/urls.py
+++ football_news_aggregator/accounts/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
 
+from football_news_aggregator.accounts.views import index
+
+urlpatterns = [
+    path('', index, name='index')
 ]
--- football_news_aggregator/accounts/views.py
+++ football_news_aggregator/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic as views
 
 # Create your views here.
+
+def index(request):
+    return render(request, template_name='common/base.html')
+
+
+class RegisterUserView(views.CreateView):
+    pass
