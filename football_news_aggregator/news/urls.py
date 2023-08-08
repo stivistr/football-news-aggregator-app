c07e4ee6c531011938a371c8994eff8671f9d539
Stivan Strahilov <stivan_strahilov@abv.bg>
2023-08-08 22:53:34 +0300

    work on articles

--- football_news_aggregator/news/urls.py
+++ football_news_aggregator/news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from football_news_aggregator.news.views import CreateArticleView
+from football_news_aggregator.news.views import CreateArticleView, article_list
 
 urlpatterns = [
     path('articles/', CreateArticleView.as_view(), name='create_article'),
+    path('all_articles/', article_list, name='list_articles'),
 ]
--- football_news_aggregator/news/views.py
+++ football_news_aggregator/news/views.py
@@ -1,9 +1,8 @@
-from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic as views
 from football_news_aggregator.news.forms import CreateArticleForm
-UserModel = get_user_model()
+from football_news_aggregator.news.models import NewsArticle
 
 
 class CreateArticleView(views.CreateView):
@@ -11,4 +10,17 @@ class CreateArticleView(views.CreateView):
     form_class = CreateArticleForm
     success_url = reverse_lazy('index')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            return super().form_valid(form)
+        else:
+            return self.form_invalid(form)
 
+
+def article_list(request):
+    articles = NewsArticle.objects.all()
+    context = {
+        'articles': articles,
+    }
+
+    return render(request, 'common/home_page.html', context)
