355640fe4af5e0ff6934c628a1150041b27d8558
Stivan Strahilov <stivan_strahilov@abv.bg>
2023-08-11 12:42:19 +0300

    changes added to NewsArticle model and new model was added

--- football_news_aggregator/news/models.py
+++ football_news_aggregator/news/models.py
@@ -2,34 +2,26 @@ from django.db import models
 
 
 class NewsArticle(models.Model):
+    TOPICS = (
+        ('Transfers', 'Transfers'),
+        ('Breaking News', 'Breaking News'),
+        ('Announcement', 'Announcement'),
+        ('Results', 'Results'),
+        ('Fixture', 'Fixture'),
+        ('Other', 'Other'),
+    )
     title = models.CharField(max_length=200)
     content = models.TextField()
+    topic = models.CharField(max_length=200, blank=True, null=True, choices=TOPICS)
     publication_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
 
-class NewsSource(models.Model):
-    name = models.CharField(max_length=100)
-    website = models.URLField()
-    description = models.TextField()
-    article = models.ForeignKey(
-        NewsArticle,
-        on_delete=models.CASCADE,
-    )
-
-    def __str__(self):
-        return self.name
+class Comment(models.Model):
+    comment_text = models.TextField(max_length=300, blank=False, null=False)
+    date_time_of_publication = models.DateTimeField(auto_now_add=True)
+    to_article = models.ForeignKey(NewsArticle, on_delete=models.CASCADE, )
 
 
-class NewsCategory(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    article = models.ForeignKey(
-        NewsArticle,
-        on_delete=models.CASCADE,
-    )
-
-    def __str__(self):
-        return self.name
